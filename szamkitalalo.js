22e807565c277aee889e5895284bca4daf296210
Zsolt Kacsor <katsa007@gmail.com>
2024-02-15 23:11:30 +0100

    Kész a játék indítás és a tippelő sorok törlésével

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -1,14 +1,33 @@
+// ! Globális változók
+
+let guessListContainer = document.querySelector(".js-guess-container");
+let currentGuessField = document.querySelector(".js-guess-field");
+let newGameButton = document.querySelector(".js-new-game-button")
+
 let target;
 
 function generateTarget() {
     return Math.floor(Math.random()* 100 +1)
 }
 
+function resetGame(){
+    //* Törli a korábbi tippeket
+    guessListContainer.innerHTML = "";
+
+    //* input mező tartalmának törlése
+    currentGuessField.innerHTML = "";
+
+
+}
+
 function startGame() {
+    resetGame();
     target = generateTarget()
 }
 
 startGame();
+newGameButton.addEventListener("click " , startGame )
+
 
 
 
